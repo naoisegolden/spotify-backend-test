c29e2143d7f075cce7f6cc8ef8d4ee16c5e93ef3
Naoise Golden Santos <naoise.golden@gmail.com>
2011-09-14 18:18:39 +0200

    Parsing complete.

--- index.php
+++ index.php
@@ -8,35 +8,92 @@ class Application extends lib {
 	/**
 	 * Given a URL it parses a set of elements and returns them.
 	 * @param String url The URL to parse
-	 * @return array of variables
+	 * @return matrix of data
 	 */ 
     private function _parse( $url = null ) {
     	if (!$url )
     		throw new Exception('No URL to parse was given.');
+    		
+    	try {
+	    	// start parsing
+	    	libxml_use_internal_errors(TRUE);
+			$dom = new DOMDocument;
+			$dom->loadHTMLFile($url);
+			libxml_clear_errors();
+			// use XPath to find all nodes with a class attribute of header
+			$xp = new DOMXpath($dom);
+			$query = '//div[@id="main"]';
+			$query = '//table/tr';
+			$elements = $xp->query($query);
+			
+    	} catch (Exception $e) {
+    		throw $e;
+    	}
+    	
+    	// we need to grab: rank, rating, title, year and number_of_votes
+		
+		// grap what we have
+		if (!is_null($elements)) {
+		  	// loop the array
+		  	$i = 0;
+		  	$data = array();
+		  	foreach ($elements as $element) {
+		  		if($i>10) break; // we stop at 10
+		  	  	if($i>0) { // first TR is header, off with his head!
+		  	  				
+				    $nodes = $element->childNodes;
+				    $j = 0;
+				    $list = array();
+				    foreach ($nodes as $node) {
+				      switch($j) {
+				      	case 0:	// rank
+				      		$list['rank'] = $node->nodeValue;
+				      		break;
+				      	case 1:	// rating
+				      		$list['rating'] = $node->nodeValue;
+				      		break;
+				      	case 2:	// title + year
+				      		$pieces = explode("(", $node->nodeValue);
+				      		$list['title'] = $pieces[0];
+				      		$list['year'] = str_replace(")","",$pieces[1]);
+				      		break;
+				      	case 3:	// number_of_votes
+				      		$list['number_of_votes'] = $node->nodeValue;
+				      		break;
+				      }
+				      $j++;
+				    }
+				    $data[] = $list;
+		  	  	}
+				$i++;
+			}
+		}
+
+    	return $data;
     }
     /**
 	 * Main function, triggers all the logic and renders.
 	 */
-    public function run() {
+    public function run($url = null) {
 
-        $var = '1st variable to use in a template';
-        $var2 = '2nd variable to use in a template';
+        $error = '';
         $message = '';
+        $data = array();
         
         try {
-    		_parse();
+    		$data = $this->_parse($url);
     	} catch (Exception $e) {
-    		$message = $e->getMessage();
+    		$error = $e->getMessage();
     	}
 
-        $this->render('main', compact('var', 'var2', 'message'));
+        $this->render('main', compact('var', 'var2', 'error', 'message', 'data'));
     }
-    
 
 }
 
-$app = new Application();
+$url = 'http://www.imdb.com/chart/top';
 
-$app->run();  
+$app = new Application();
+$app->run($url);  
 
 ?>
--- views/main.php
+++ views/main.php
@@ -7,8 +7,10 @@
   <meta charset="utf-8">
   <meta name="author" content="Naoise Golden Santos">
   <style type="text/css">
-  	#container { width: 980; margin: 0 auto; background-color: #ccc; border-right: 1px solid #333; border-left: 1px solid #333; }
-  	.message { border: 1px solid red; background-color: lightRed; padding: 10px; width: 100%; }
+  	html,body { height: 100%; width: 100%; margin: 0; }
+  	#container { width: 960px; height: 100%; margin: 0 auto; background-color: #eee; border-right: 1px solid #ccc; border-left: 1px solid #ccc; padding: 20px; }
+  	.error, .message { border: 1px solid red; background-color: lightPink; width: 100%; margin-bottom: 20px; }
+  	.error span, .message span { padding: 10px; display: block; }
   </style>
   <script type="text/javascript">
   	/* js */
@@ -21,9 +23,30 @@
     <header>
     </header>
     <div id="main" role="main">
-    	<?php if (!empty($message)) { ?><div class="message"><?php echo $message; ?></div><?php } ?>
-    	<p><?php echo $var; ?></p>
-    	<p><?php echo $var2 ?></p>
+    	<?php if (!empty($error)) { ?><div class="error"><span><?php echo $error; ?></span></div><?php } ?>
+    	<?php if (!empty($message)) { ?><div class="message"><span><?php echo $message; ?></span></div><?php } ?>
+    	
+    	<h2>Current IMDB Top 10 Chart</h2>
+    	<table>
+    		<thead>
+    			<tr>
+    				<th>Rank</th>
+    				<th>Rating</th>
+    				<th>Title</th>
+    				<th>Year</th>
+    				<th>Num. of votes</th>
+    			</tr>
+    		</thead>
+    		<tbody>
+    			<?php foreach ( $data as $item ): ?>
+    			<tr>
+    				<?php foreach ( $item as $element ): ?>
+	    			<td><?php echo $element; ?></td>
+	    			<?php endforeach; ?>
+    			</tr>
+    			<?php endforeach; ?>
+    		</tbody>
+    	</table>
     </div><!-- /#main -->
     <footer>
     </footer>
